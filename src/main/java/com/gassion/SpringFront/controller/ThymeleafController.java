63681e14b97ceb2e918ffc797647f99cb6f7384f
Soslan Gassiev <soslangassity04@gmail.com>
2023-06-05 22:37:39 +0300

    feat : add page /th

--- src/main/java/com/gassion/SpringFront/controller/ThymeleafController.java
+++ src/main/java/com/gassion/SpringFront/controller/ThymeleafController.java
@@ -1,15 +1,24 @@
 package com.gassion.SpringFront.controller;
 
+import com.gassion.SpringFront.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/th")
 public class ThymeleafController {
     @GetMapping
-    public String getIndex() {
+    public String getIndex(Model model) {
+        User user = new User(1, "Soslan", "Gassiev",
+                22, "79298119014", "soslangassity04@gmail.com", Arrays.asList("Java", "Scala", "Kotlin"));
+        model.addAttribute("user", user);
+        System.out.println(user.getLang());
         return "thymeleaf_front";
     }
-
 }
